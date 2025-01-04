0684573b7a45d6760e1f91e241bb72809256ac5b
Subbareddy Changa <csrcse509@gmail.com>
2025-01-04 13:00:37 +0530

    Removing ZeroStep script and added a normal script for parallel run in GitHub Actions

--- tests/githubIntegration.spec.js
+++ tests/githubIntegration.spec.js
@@ -1,6 +1,4 @@
-import { test, expect } from '@playwright/test'
-import { ai } from '@zerostep/playwright'
-//const { test, expect } = require('@playwright/test'); //Kind of Importing a package or test annotation
+const { test, expect } = require('@playwright/test'); //Kind of Importing a package or test annotation
 
 //test.only annotation helps in running only a particular test in a file.
 test('Launch Google with new browser instance', async ({ browser }) => {
@@ -14,24 +12,11 @@ test('Launch Google with new browser instance', async ({ browser }) => {
     await expect(page).toHaveTitle("Google");
   });
 
-test.describe('AI Registration submission', () => {
-    test('Sample Registration form', async ({ page }) => {
-        await page.goto('file:///C:/Users/suvic/Downloads/SampleRegistrationForm.html');
-        await ai('Input in the First Name field as Tester', { page, test })
-        await ai('Input in the Last Name field as Testing', { page, test })
-        await ai('Input the Email with a realistic email id', { page, test })
-        await ai('Input the Password with a realistic password', { page, test })
-        await ai('Select Gender as Male from the dropdown', { page, test })
-        await ai('Select a random date in Date of Birth field', { page, test })
-    });
-});
-
-test.describe('AI Form submission', () => {
-    test('Sample HTML form submission', async ({ page }) => {
-        await page.goto('file:///C:/Users/suvic/Downloads/DynamicSampleForm.html');
-        await ai('Fill out the text fields with realistic values', { page, test })
-        await ai('Select one of the radio button value', { page, test })
-        await ai('Select all of the checkbox values', { page, test })
-        await ai('Submit the form', { page, test })
-    });
-});
+  test('Click Google Search Results', async ({ page }) => {
+    await page.goto('https://www.google.com/');
+    //await page.getByLabel('Search', { exact: true }).click();
+    await page.getByLabel('Search', { exact: true }).fill('PlayWright Automation');
+    await page.getByRole('button', { name: 'Google Search' }).click();
+    await page.locator("(//div[@id='search']//a)[1]").click();
+    await page.locator("(//*[contains(@class,'navbar__title') and text()='Playwright'])[1]").isVisible();
+  });
