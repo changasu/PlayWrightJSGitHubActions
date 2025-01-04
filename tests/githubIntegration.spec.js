251a736e78ec7f0d69d9717bee278a7ab7ff0bdf
Subbareddy Changa <csrcse509@gmail.com>
2025-01-04 12:49:05 +0530

    zerostep Integration and Multipe test parallel run test in GitHub Actions

--- tests/githubIntegration.spec.js
+++ tests/githubIntegration.spec.js
@@ -1,4 +1,6 @@
-const { test, expect } = require('@playwright/test'); //Kind of Importing a package or test annotation
+import { test, expect } from '@playwright/test'
+import { ai } from '@zerostep/playwright'
+//const { test, expect } = require('@playwright/test'); //Kind of Importing a package or test annotation
 
 //test.only annotation helps in running only a particular test in a file.
 test('Launch Google with new browser instance', async ({ browser }) => {
@@ -11,3 +13,25 @@ test('Launch Google with new browser instance', async ({ browser }) => {
     console.log(await page.title());
     await expect(page).toHaveTitle("Google");
   });
+
+test.describe('AI Registration submission', () => {
+    test('Sample Registration form', async ({ page }) => {
+        await page.goto('file:///C:/Users/suvic/Downloads/SampleRegistrationForm.html');
+        await ai('Input in the First Name field as Tester', { page, test })
+        await ai('Input in the Last Name field as Testing', { page, test })
+        await ai('Input the Email with a realistic email id', { page, test })
+        await ai('Input the Password with a realistic password', { page, test })
+        await ai('Select Gender as Male from the dropdown', { page, test })
+        await ai('Select a random date in Date of Birth field', { page, test })
+    });
+});
+
+test.describe('AI Form submission', () => {
+    test('Sample HTML form submission', async ({ page }) => {
+        await page.goto('file:///C:/Users/suvic/Downloads/DynamicSampleForm.html');
+        await ai('Fill out the text fields with realistic values', { page, test })
+        await ai('Select one of the radio button value', { page, test })
+        await ai('Select all of the checkbox values', { page, test })
+        await ai('Submit the form', { page, test })
+    });
+});
